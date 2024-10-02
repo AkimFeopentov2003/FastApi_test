e3eb99371e86da9ecfd76c7ee5eebb2dec97324a
AkimFeopentov2003 <akimses@gmail.com>
2024-10-02 17:16:22 +0300

    create logic database

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -1,6 +1,6 @@
 from fastapi import  APIRouter
 from app.bookings.dao import BookingDAO
-
+from app.bookings.schemas import SBooking
 
 router =APIRouter(
     prefix="/bookings",
@@ -8,5 +8,5 @@ router =APIRouter(
 )
 
 @router.get("")
-async def get_bookings():
+async def get_bookings() ->list[SBooking]:
     return await BookingDAO.find_all()
